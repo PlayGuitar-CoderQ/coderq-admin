c2cf4e351250a0469c5242b055b0e79d877eaacc
1282117217@qq.com <1282117217@qq.com>
2021-06-08 00:50:49 +0800

    feat(vite.config.js): ⚡️新增vite-plugin-imp 插件优化antd样式按需加载

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import vitePluginImp from 'vite-plugin-imp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/lib/${name}/style/index.less`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      },
+    },
+  },
   // 项目启动自动打开
   server: {
-    open: '/'
-  }
-})
+    open: '/',
+  },
+});
