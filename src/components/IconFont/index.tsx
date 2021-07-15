aad0af835c37d030b9d6c51888be84e472d35989
coder_Q <1282117217@qq.com>
2021-07-15 20:53:40 +0800

    feat: 🚑 替换icon链接,引入了新的图标

--- src/components/IconFont/index.tsx
+++ src/components/IconFont/index.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
 import { createFromIconfontCN } from '@ant-design/icons';
+import { IconFontUrl } from '@/enums/appEnum';
 import { Prop } from './prop';
 
 const Icon: FC<Prop> = (props: Prop) => {
   const IconFont = createFromIconfontCN({
-    scriptUrl: '//at.alicdn.com/t/font_2622053_m4139rs6k3q.js',
+    scriptUrl: IconFontUrl as unknown as string,
   });
   return <IconFont type={props.type} style={props.style} />;
 };
--- src/enums/appEnum.ts
+++ src/enums/appEnum.ts
@@ -7,3 +7,7 @@ export enum TypeWriterTextEnum {
   FIRSTEXT = '弹吉他的CoderQ',
   ENDTEXT = '欢迎回来👽️🤡🧐：',
 }
+
+export enum IconFontUrl {
+  ICONURL = '//at.alicdn.com/t/font_2622053_weo5l7kgqc.js',
+}
