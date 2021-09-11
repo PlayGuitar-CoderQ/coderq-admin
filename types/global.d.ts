aaf24abbb269b5fcc44954b67320972f53a7ca6e
Coder_Q <1282117217@qq.com>
2021-09-11 23:54:35 +0800

    feat: ✨增加类型工具

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,6 @@
 declare type Nullable<T> = T | null;
 declare type Recordable<T = any> = Record<string, T>;
+declare type isAny<T> = false extends true & T ? true : false;
 
 declare interface ViteEnv {
   VITE_PORT: number;
