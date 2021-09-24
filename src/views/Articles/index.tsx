ef82d1c50d63c692ff814790f032aada54dd0246
Coder_Q <1282117217@qq.com>
2021-09-24 23:56:18 +0800

    style: 💅新增文章菜单模块

---
+++ src/views/Articles/index.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+
+import React from 'react';
+
+const Articles: FC = () => {
+  return (
+    <div>
+      <span>Articles</span>
+    </div>
+  );
+};
+
+export default Articles;
